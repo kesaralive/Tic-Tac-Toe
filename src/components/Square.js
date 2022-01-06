47e9a1f78cebd2560e454fcfdbb978a8bbf3bc95
kesaralive <kesaralive@gmail.com>
2022-01-06 10:08:07 +0530

    Lift Square's State Up to the Board Component

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,11 @@
 import React from "react";
 class Square extends React.Component {
   render() {
-    return <button className="square">{this.props.value}</button>;
+    return (
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
+      </button>
+    );
   }
 }
 export default Square;
