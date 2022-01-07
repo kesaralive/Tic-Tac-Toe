03c4ca1517d18901cd8282a28a0714b0553fad06
kesaralive <kesaralive@gmail.com>
2022-01-07 10:54:38 +0530

    Using React-bootstrap on tic tac toe

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Game from "./components/Game";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <Game />
+      {/* <Footer /> */}
     </div>
   );
 }
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,8 @@
 function Square(props) {
   return (
     <button className="square" onClick={() => props.onClick()}>
-      {props.value}
+      {props.value === "X" ? <i className="fas fa-times ttc-txt-primary"></i> : null}
+      {props.value === "O" ? <i className="fas fa-dot-circle ttc-txt-primary"></i> : null}
     </button>
   );
 }
